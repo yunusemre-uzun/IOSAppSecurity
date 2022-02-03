b2c6e83e4bb2858c987cccd04a5a4c9acc8e57cb
Yunus Emre Uzun <yunus.uzun@metu.edu.tr>
2022-02-03 15:19:43 +0300

    Initial Commit

---
+++ iosAppSecurity/iosAppSecurity.h
@@ -0,0 +1,18 @@
+//
+//  iosAppSecurity.h
+//  iosAppSecurity
+//
+//  Created by Uzun, Yunus on 3.02.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iosAppSecurity.
+FOUNDATION_EXPORT double iosAppSecurityVersionNumber;
+
+//! Project version string for iosAppSecurity.
+FOUNDATION_EXPORT const unsigned char iosAppSecurityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iosAppSecurity/PublicHeader.h>
+
+
