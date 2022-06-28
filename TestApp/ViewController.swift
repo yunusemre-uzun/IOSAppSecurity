7eecb3ff63d2d0f33054ad2b2a63c4c7749f4be0
Yunus Emre Uzun <yunus.uzun@metu.edu.tr>
2022-06-28 14:46:21 +0300

    add test app

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Uzun, Yunus on 7.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
